91c4cb5e2b5d6e71e473cd6a15cd193bbedcd22e
potapovDim <potapovd3012@gmail.com>
2017-06-21 18:41:28 +0300

    unit tests, React to global, navitem test

--- test/.setup.js
+++ test/.setup.js
@@ -1,12 +1,13 @@
 import { expect } from 'chai';
 import { jsdom } from 'jsdom';
 import register from 'ignore-styles';
-import hook from 'css-modules-require-hook';
-import { shallow, mount } from 'enzyme';
+import React from 'react'
+register(['.sass', '.scss', '.css']);
 
 const exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('<body><div id="app"></div></body>');
+global.React = React;
+global.document = jsdom('');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
