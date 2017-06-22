6d2558733a5efabf6ec94871d5e12a43946085e8
potapovDim <potapovd3012@gmail.com>
2017-06-22 12:26:21 +0300

    remove React from global

--- test/.setup.js
+++ test/.setup.js
@@ -1,12 +1,11 @@
 import { expect } from 'chai';
 import { jsdom } from 'jsdom';
 import register from 'ignore-styles';
-import React from 'react'
+
 register(['.sass', '.scss', '.css']);
 
 const exposedProperties = ['window', 'navigator', 'document'];
 
-global.React = React;
 global.document = jsdom('');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
